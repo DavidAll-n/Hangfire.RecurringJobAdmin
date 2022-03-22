1bbfc3cdfc8e638a29b1c5ffb555809bb9fe40e1
David Allen <david.allen@petdesk.com>
2022-03-22 11:51:09 -0400

    rename solution and projects, update references

--- src/PetDesk.Hangfire.RecurringJobAdmin/Pages/JobExtensionPage.cs
+++ src/PetDesk.Hangfire.RecurringJobAdmin/Pages/JobExtensionPage.cs
@@ -1,26 +1,28 @@
 ﻿using Hangfire.Dashboard;
 using Hangfire.Dashboard.Pages;
 using Hangfire.RecurringJobAdmin.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Hangfire.RecurringJobAdmin.Pages
 {
-    internal sealed class JobsStoppedPage : PageBase
+    internal sealed class JobExtensionPage : PageBase
     {
-        public const string Title = "Stopped Jobs";
-        public const string PageRoute = "/jobs/stopped";
+        public const string Title = "Job Configuration";
+        public const string PageRoute = "/JobConfiguration";
 
         private static readonly string PageHtml;
 
-        static JobsStoppedPage()
+        static JobExtensionPage()
         {
-            PageHtml = Utility.ReadStringResource("Hangfire.RecurringJobAdmin.Dashboard.JobsStopped.html");
+            PageHtml = Utility.ReadStringResource("PetDesk.Hangfire.RecurringJobAdmin.Dashboard.JobExtension.html");
         }
 
         public override void Execute()
         {
             WriteEmptyLine();
             Layout = new LayoutPage(Title);
-            Write(Html.JobsSidebar());
             WriteLiteralLine(PageHtml);
             WriteEmptyLine();
         }
